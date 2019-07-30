e77fa54dba1548ef865ff35f3203bdcc2581c05c
semenovae3 <semenovae3@alfastrah.ru>
2019-07-30 12:55:49 +0700

    master :: add fill s_store via migration, problems with test db => fail tests

---
+++ src/main/kotlin/com/dokhabackend/dokha/config/JpaConfig.java
@@ -0,0 +1,69 @@
+package com.dokhabackend.dokha.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+
+/**
+ * Created by SemenovAE on 29.07.2019
+ */
+
+@Configuration
+@EnableTransactionManagement
+public class JpaConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.hikari.maximumPoolSize:5}")
+    private String maximumPoolSize;
+
+    @Bean
+    public DataSource configureDataSource() {
+//        LOGGER.debug("Configuring datasource {} {} {}", driverClassName, url, user);
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driverClassName);
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPoolName("DokhaCP");
+        config.setPassword(password);
+        config.setMaximumPoolSize(Integer.parseInt(maximumPoolSize));
+        config.setAutoCommit(true);
+        config.setConnectionTimeout(0);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource hikariDataSource = new HikariDataSource(config);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(hikariDataSource);
+
+        try {
+            jdbcTemplate.execute("CREATE SCHEMA IF NOT EXISTS DOKHA;");
+        } catch (Exception e) {
+//            log.error(e.getMessage());
+        } finally {
+            hikariDataSource.close();
+        }
+
+        config.setAutoCommit(false);
+        return new HikariDataSource(config);
+    }
+}
---
+++ src/main/resources/db/changelog/v0.0.1/data/init.sql
@@ -0,0 +1,126 @@
+SET search_path = dokha;
+
+--Заведение
+create table dokha.s_store
+(
+    id       bigserial not null
+        constraint s_store_pkey
+            primary key,
+    location varchar(255),
+    name     varchar(255),
+    photo    bytea
+);
+
+--Место резерва (столики)
+create table dokha.s_place_reservation
+(
+    id          bigserial not null
+        constraint s_place_reservation_pkey
+            primary key,
+    description varchar(255),
+    seats_count integer,
+    store_id    bigint
+        constraint s_place_reservation_store_fk
+            references s_store
+);
+
+
+--Справочник дни недели
+create table dokha.s_day_of_week
+(
+    id   bigserial not null
+        constraint s_day_of_week_pkey
+            primary key,
+    name varchar(255)
+);
+
+--Конфиг расписаний
+create table dokha.s_timetable_config
+(
+    id             bigserial not null
+        constraint s_timetable_config_pkey
+            primary key,
+    end_time       bigint,
+    start_time     bigint,
+    day_of_week_id bigint
+        constraint s_timetable_config_day_of_week_fk
+            references s_day_of_week,
+    store_id       bigint
+        constraint s_timetable_config_store_fk
+            references s_store
+);
+
+--Расписание заведения
+create table dokha.timetable
+(
+    id           bigserial not null
+        constraint timetable_pkey
+            primary key,
+    end_time     bigint,
+    start_time   bigint,
+    working_date bigint,
+    working_day  boolean,
+    store_id     bigint
+        constraint timetable_store_fk
+            references s_store
+);
+
+--Пользователи
+create table dokha."user"
+(
+    id       bigserial not null
+        constraint user_pkey
+            primary key,
+    login    varchar(255),
+    password varchar(255)
+);
+
+--Роли пользователя
+create table dokha.user_role
+(
+    user_id bigint not null
+        constraint user_role_user_fk
+            references "user",
+    roles   integer
+);
+
+--Брони
+create table dokha.reservation
+(
+    id               bigserial not null
+        constraint reservation_pkey
+            primary key,
+    reservation_time bigint,
+    place_id         bigint
+        constraint s_place_reservation_reservation_fk
+            references s_place_reservation,
+    timetable_id     bigint
+        constraint timetable_reservation_fk
+            references timetable,
+    user_id          bigint
+        constraint user_reservation_fk
+            references "user"
+);
+
+--Должеости персонала
+create table dokha.s_personal_position
+(
+    id   bigserial not null
+        constraint s_personal_position_pkey
+            primary key,
+    name varchar(255)
+);
+
+--Персонал
+create table dokha.s_personal
+(
+    id          bigserial not null
+        constraint s_personal_pkey
+            primary key,
+    age         integer,
+    first_name  varchar(255),
+    last_name   varchar(255),
+    position_id bigint
+        constraint s_personal_position_personal_fk
+            references s_personal_position
+);
