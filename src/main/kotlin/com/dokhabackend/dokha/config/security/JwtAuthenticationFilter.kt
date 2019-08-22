154549dad6d32eb892439259c73fe68701a9108a
Александр <aesemenov95@gmail.com>
2019-08-22 23:06:38 +0700

    master :: FUCKING AUTH I'M WIN!

--- src/main/kotlin/com/dokhabackend/dokha/config/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/dokhabackend/dokha/config/security/JwtAuthenticationFilter.kt
@@ -1,8 +1,9 @@
 package com.dokhabackend.dokha.config.security
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.RememberMeAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.security.sasl.AuthenticationException
@@ -19,26 +20,24 @@ import javax.servlet.http.HttpServletResponse
 class JwtAuthenticationFilter : OncePerRequestFilter() {
 
     @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+
+    @Autowired
     private lateinit var jwtProperties: JwtProperties
 
     override fun doFilterInternal(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
-        val header = req.getHeader(jwtProperties.header)
+        val token = req.getHeader(jwtProperties.header)
         val uri = req.requestURI
 
-        if (uri.contains("register") || uri.contains("login")) {
+        if (uri.contains("register") || uri.contains("login"))
+            chain.doFilter(req, res)
+        else if (token == null || token.isEmpty())
+            throw AuthenticationException("Вася а где токен?")
 
-        } else {
+        val login = JwtTokenUtil(jwtProperties).getUsernameFromToken(token)
+        val user = userDetailsService.loadUserByUsername(login)
 
-            if (header == null || header.isEmpty())
-                throw AuthenticationException("Вася а где токен?")
-
-            val login = JwtTokenUtil(jwtProperties).getUsernameFromToken(header)
-
-            val userDetails = SecurityContextHolder.getContext().authentication.principal as UserDetails
-
-            if (userDetails.username != login)
-                throw AuthenticationException("Пользователь не авторизован")
-        }
+        SecurityContextHolder.getContext().authentication = RememberMeAuthenticationToken(token, user, user.authorities)
 
         chain.doFilter(req, res)
     }
--- src/main/kotlin/com/dokhabackend/dokha/config/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/dokhabackend/dokha/config/security/WebSecurityConfig.kt
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
@@ -22,30 +24,28 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-class WebSecurityConfig
-@Autowired constructor(private val exceptionHandler: BaseExceptionHandler) : WebSecurityConfigurerAdapter() {
-
-    @Qualifier("userServiceImpl")
-    @Autowired
-    private lateinit var userDetailsService: UserDetailsService
-
-    @Autowired
-    fun globalUserDetails(auth: AuthenticationManagerBuilder) {
-        auth.userDetailsService<UserDetailsService>(userDetailsService)
-                .passwordEncoder(encoder())
-    }
-
-    @Bean
-    override fun authenticationManagerBean(): AuthenticationManager = super.authenticationManagerBean()
+class WebSecurityConfig(
+        private val exceptionHandler: BaseExceptionHandler
+)
+    : WebSecurityConfigurerAdapter() {
 
     @Bean
     fun authenticationTokenFilterBean(): JwtAuthenticationFilter = JwtAuthenticationFilter()
 
+    @Bean
+    fun encoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager = super.authenticationManagerBean()
+
     override fun configure(http: HttpSecurity) {
         http
-                .cors().and().csrf().disable()
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers("/login", "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/login", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
@@ -54,6 +54,5 @@ class WebSecurityConfig
                 .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter::class.java)
     }
 
-    @Bean
-    fun encoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+
 }
